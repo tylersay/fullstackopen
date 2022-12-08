7c29db03fb63b4f76e88e1fe277482010536c72a
unknown <sayqi@live.com>
2022-12-08 13:06:25 +0800

    Exercise 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,23 +2,31 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const Countries = (props) => {
+  // console.log('props.countriesToShow', props.countriesToShow)
+  
   if (props.countriesToShow.length > 10) {
     return (
       <TooMany />
     )
   }
   if (props.countriesToShow.length > 1) {
+    
+    // setDisplayCountries(props.countriesToShow)
     return (
       <ul>
         {props.countriesToShow.map(country =>
-          <TwoToTen country={country.name.common}
-            key={props.countriesToShow.indexOf(country)} />)}
+          <TwoToTen country={country}
+            key={props.countriesToShow.indexOf(country)}
+            countriesToShow={props.countriesToShow} />)}
+
       </ul>
     )
   }
-  return (
-    <OneCountry country={props.countriesToShow} />
-  )
+  if (props.countriesToShow.length === 1) {
+    return (
+      <OneCountry country={props.countriesToShow} />
+    )
+  }
 }
 
 const TooMany = () => {
@@ -28,23 +36,30 @@ const TooMany = () => {
 }
 
 const TwoToTen = (props) => {
+  const [showOne, setShowOne] = useState(false)
+
+  const handleShow = (event) => {
+    console.log('props.country', props.country)
+    setShowOne(!showOne)
+  }
   return (
     <li>
-      {props.country}
+      {props.country.name.common} <button onClick={handleShow}>show</button>
+      {showOne && <OneCountry country={[props.country]} />}
     </li>
   )
 }
 
 const OneCountry = (country) => {
   // console.log('oneCountry', country.country[0].name.common)
-  console.log('oneCountry', country)
+  // console.log('oneCountry', country)
   const heroCountry = country.country[0]
   console.log('heroCountry', heroCountry)
-  console.log('heroCountry', heroCountry.flags.png)
+  // console.log('heroCountry', heroCountry.flags.png)
   const flag = heroCountry.flags.png
   return (
     <>
-     
+
       <h1>{heroCountry.name.common}</h1>
       captial: {heroCountry.capital}
       <br />
@@ -53,11 +68,11 @@ const OneCountry = (country) => {
       <h2>languages:</h2>
       <ul>
         {Object.values(heroCountry.languages).map(language =>
-          <li>{language}</li>
+          <li key={language}>{language} </li>
         )}
       </ul>
 
-      <img src={flag} alt="country flag"/>
+      <img src={flag} alt="country flag" />
     </>
   )
 }
@@ -68,22 +83,23 @@ function App() {
   const [countries, setCountries] = useState([])
 
 
+
   const handleFilterField = event => {
     setFindCountries(event.target.value)
   }
 
   useEffect(() => {
-    console.log("effect")
+    // console.log("effect")
     axios
       .get("https://restcountries.com/v3.1/all")
       .then(response => {
-        console.log('promise fulfilled')
+        // console.log('promise fulfilled')
         setCountries(response.data)
-        console.log('response.data', response.data)
+        // console.log('response.data', response.data)
       })
   }, [])
 
-  let countriesToShow = countries.filter(country =>
+  const countriesToShow = countries.filter(country =>
     country.name.common.toUpperCase()
       .includes(findCountries.trim().toUpperCase()))
 
@@ -99,7 +115,8 @@ function App() {
         </input>
       </form>
       <p />
-      <Countries countriesToShow={countriesToShow} />
+      <Countries countriesToShow={countriesToShow}
+      />
     </>
   );
 }
